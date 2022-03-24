fc55efd45aa04740e06b4bd0af7990f491e877cd
Atlas Sproson <atlas.sproson@r2-factory.com>
2022-03-24 13:51:44 +0000

    Completes lesson 5, API calls with Axios

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # real-world-vue
+
+https://my-json-server.typicode.com/asproson/json-server/events
--- real-world-vue/src/views/EventList.vue
+++ real-world-vue/src/views/EventList.vue
@@ -8,6 +8,7 @@
 <script>
 // @ is an alias to /src
 import EventCard from '@/components/EventCard.vue'
+import EventService from '@/services/EventService.js'
 
 export default {
   name: 'EventList',
@@ -16,42 +17,17 @@ export default {
   },
   data() {
     return {
-      events: [
-        {
-          id: 5928101,
-          category: 'animal welfare',
-          title: 'Cat Adoption Day',
-          description: 'Find your new feline friend at this event.',
-          location: 'Meow Town',
-          date: 'January 27, 2022',
-          time: '12:00',
-          petsAllowed: true,
-          organizer: 'Kay Laydee',
-        },
-        {
-          id: 5928273,
-          category: 'food',
-          title: 'Community Gardening',
-          description: 'Planting edible plants',
-          location: 'Floracity',
-          date: 'January 31, 2022',
-          time: '14:00',
-          petsAllowed: true,
-          organizer: 'Sharon Meese',
-        },
-        {
-          id: 592781231,
-          category: 'sustainability',
-          title: 'Beach Cleanup',
-          description: 'Help us clean up the shore.',
-          location: 'St. Tropez',
-          date: 'February 14th, 2022',
-          time: '10:00',
-          petsAllowed: true,
-          organizer: 'Carey Wales',
-        },
-      ],
+      events: null,
     }
+  },
+  created() {
+    EventService.getEvents()
+      .then((response) => {
+        this.events = response.data
+      })
+      .catch((error) => {
+        console.log(error)
+      })
   },
 }
 </script>
