36a015ac85abacdd90c689593d014414080226ce
Atlas Sproson <atlas.sproson@r2-factory.com>
2022-04-04 09:20:56 +0100

    Adds nested routing

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -9,26 +9,24 @@
         :to="{ name: 'EventList', query: { page: page - 1 } }"
         rel="prev"
         v-if="page != 1"
-        >&#60; Previous
-      </router-link>
+        >&#60; Previous</router-link
+      >
 
       <router-link
         id="page-next"
         :to="{ name: 'EventList', query: { page: page + 1 } }"
         rel="next"
         v-if="hasNextPage"
-        >&#62; Next
-      </router-link>
+        >Next &#62;</router-link
+      >
     </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
 import EventCard from '@/components/EventCard.vue'
 import EventService from '@/services/EventService.js'
 import { watchEffect } from 'vue'
-
 export default {
   name: 'EventList',
   props: ['page'],
@@ -56,7 +54,7 @@ export default {
   },
   computed: {
     hasNextPage() {
-      let totalPages = Math.ceil(this.totalEvents / 2)
+      var totalPages = Math.ceil(this.totalEvents / 2)
       return this.page < totalPages
     },
   },
@@ -69,22 +67,18 @@ export default {
   flex-direction: column;
   align-items: center;
 }
-
 .pagination {
   display: flex;
   width: 290px;
 }
-
-.pagtination a {
+.pagination a {
   flex: 1;
   text-decoration: none;
   color: #2c3e50;
 }
-
 #page-prev {
   text-align: left;
 }
-
 #page-next {
   text-align: right;
 }
